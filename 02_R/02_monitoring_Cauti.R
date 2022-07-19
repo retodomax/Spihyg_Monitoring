e411154ad06872f9bc168c4dd78558bd1dce0ce1
Reto <retozihlmann@outlook.com>
2022-07-19 14:27:02 +0200

    Cauti barplots with function instead of manual

--- 02_R/02_monitoring_Cauti.R
+++ 02_R/02_monitoring_Cauti.R
@@ -79,44 +79,10 @@ p_anyIndi <- plot_prop_period(prop_period = t_anyIndi,
 
 # Indication Barplot ------------------------------------------------------
 
-## Aggregated Barplot
-ag_bar <- dat %>% 
-  ggplot(aes(x = Messperiode, fill = Indikation)) +
-  geom_bar(position = "fill", width = .5) + 
-  scale_x_discrete(drop=FALSE) +
-  theme_bw() +
-  ylab("Anteil Beobachtungen") +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("03_figures/02_Cauti_02_Bar_agg.png", plot = ag_bar,
-       width = 16, height = 8, units = "cm", scale = 1.5)
 
-## by Barplot
-dat_agg <- dat %>% 
-  group_by(Messperiode, Klinik) %>% 
-  summarise(n = n())
+ag_bar <- barplot_agg(dat = dat, response = "Indikation",
+                      fileprefix = "03_figures/02_Cauti_02")
 
-myclin <- dat_agg %>% filter(Messperiode == max_per, n > 9) %>% pull(Klinik)
-dat2 <- dat %>% 
-  filter(Klinik %in% myclin)
-dat_agg <- dat_agg %>% filter(Klinik %in% myclin)
-
-cols2 <- c("USZ_Durchschnitt" = "lightgray")
-p <- dat2 %>% 
-  ggplot(aes(x = Messperiode)) +
-  geom_area(data = t_anyIndi[[2]],
-            mapping = aes(x = Messperiode, y = percent_correct, group = 1,
-                          fill = "USZ_Durchschnitt")) + 
-  scale_fill_manual(name = "", values = cols2) +
-  new_scale_fill() +
-  geom_bar(aes(fill = Indikation), position = "fill", width = .5) +
-  scale_x_discrete(drop=FALSE) +
-  theme_bw() +
-  ylab("Anteil Beobachtungen") +
-  scale_y_continuous(breaks = seq(0,1,0.25), limits = c(-.3,1)) +
-  geom_text(data = dat_agg, aes(label = n, y = -.25, x = Messperiode)) +
-  annotate(geom="text", x= 0.8, y=-.1, label="Gesamt N",
-           hjust = 0) +
-  facet_wrap(~ Klinik) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
-ggsave("03_figures/02_Cauti_02_Bar_by.png", plot = p,
-       width = 24.7, height = 20, units = "cm", scale = 1.5)
+barplot_by(dat = dat, response = "Indikation", by = "Klinik"
+           , fileprefix = "03_figures/02_Cauti_02", n_min = 10
+           , gray_area_dat = t_anyIndi[[2]])
