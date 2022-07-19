47809216e44c12db72494be3e54df5d27d628ea0
Reto <retozihlmann@outlook.com>
2022-07-19 14:23:18 +0200

    ad runAll script

---
+++ 02_R/00_runAll.R
@@ -0,0 +1,18 @@
+#' ---
+#' project: Zivi2022R    ##################################################
+#' title:   Make all plots
+#' author:  Reto Zihlmann <retozihlmann@outlook.com>
+#' date:    2022-07-18 16:11:49
+#' output:  github_document   #############################################
+#' ---
+
+
+source("02_R/00_fun.R")
+source("02_R/01_monitoring_VAP5.R")
+source("02_R/02_monitoring_Cauti.R")
+source("02_R/03_monitoring_HDM.R")
+source("02_R/04_monitoring_Clabsi.R")
+source("02_R/05_ErschBild.R")
+# source("02_R/")
+# source("02_R/")
+# source("02_R/")
